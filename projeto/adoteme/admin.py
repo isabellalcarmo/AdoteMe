db053e41c3079e7d60661632cb47e6e37acfd226
sonnenblumectrl <isabella.carmo@hotmail.com>
2022-10-22 17:16:16 -0300

    models + registros do admin + autorização do usuário

--- projeto/adoteme/admin.py
+++ projeto/adoteme/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Usuario, Estado, Unidade, Animal, ListaAdocao
 
-# Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Estado)
+admin.site.register(Unidade)
+admin.site.register(Animal)
+admin.site.register(ListaAdocao)
--- projeto/adoteme/models.py
+++ projeto/adoteme/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Usuario(AbstractUser):
+    cpf = models.CharField(max_length=11)
+
+class Estado(models.Model):
+    estado_id = models.AutoField(primary_key=True)
+    nome_estado = models.CharField(max_length=100, unique=True)
+
+class Unidade(models.Model):
+    unidade_id = models.AutoField(primary_key=True)
+    estado = models.ForeignKey('Estado', on_delete=models.CASCADE, db_column='fk_estado_id', related_name='unidade_estado_fk')
+    responsavel = models.ForeignKey('Usuario',on_delete=models.CASCADE, db_column='fk_responsavel_id', related_name='unidade_responsavel_fk')
+    nome_unidade = models.CharField(max_length=200, blank=True, null=True)
+    telefone_unidade = models.CharField(max_length=20, blank=True, null=True)
+    descricao_unidade = models.CharField(max_length=200, blank=True, null=True)
+
+class Animal(models.Model):
+    animal_id = models.AutoField(primary_key=True)
+    unidade = models.ForeignKey('Unidade',on_delete=models.CASCADE, db_column='fk_unidade_id', related_name='animal_unidade_fk')
+    nome_animal = models.CharField(max_length=100, blank=True, null=True)
+    tipo_animal = models.CharField(max_length=100, blank=True, null=True)
+    descricao_animal = models.CharField(max_length=200, blank=True, null=True)
+
+class ListaAdocao(models.Model):
+    lista_id = models.AutoField(primary_key=True)
+    animal = models.ForeignKey('Animal', on_delete=models.CASCADE, db_column='fk_animal_id', related_name='lista_animal_fk')
+    adotante = models.ForeignKey('Usuario',on_delete=models.CASCADE, db_column='fk_responsavel_id', related_name='lista_responsavel_fk')
+
+    class Meta:
+        unique_together = ('animal', 'adotante')
+
