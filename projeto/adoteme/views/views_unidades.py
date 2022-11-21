65aed00f5fab5f1ce85b18399ca5d50fb349bb75
sonnenblumectrl <isabella.carmo@hotmail.com>
2022-11-21 20:13:17 -0300

    ajuste na visualização da unidade

--- projeto/adoteme/views/views_unidades.py
+++ projeto/adoteme/views/views_unidades.py
@@ -94,10 +94,13 @@ def editar_unidade(request, unidade_id):
 def visualizar_unidade(request, unidade_id):
     unidade = get_object_or_404(Unidade, unidade_id=unidade_id)
     animais = Animal.objects.filter(unidade=unidade_id, adotado=False).all()
-    animais_lista_adotados = ListaAdocao.objects.filter(adotante=request.user, adotante_adotou=True, animal__unidade__unidade_id=unidade_id).all()
+    animais_lista_adotados = ListaAdocao.objects.filter(adotante_adotou=True, animal__unidade__unidade_id=unidade_id).all()
 
     if animais_lista_adotados.count() == 0:
+        print('aaaa')
         animais_lista_adotados == False
+
+    print(animais_lista_adotados)
 
     context = {
         'unidade': unidade,
